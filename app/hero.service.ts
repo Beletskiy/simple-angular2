6cd1e079997268a1322bdf980daf9ea28f2faeb6
Beletskiy <Alexey1Beletskiy@gmail.com>
2016-04-01 16:08:24 +0300

    tour of hero finish

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,21 @@
 import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
     getHeroes() {
-        return HEROES;
+        //return HEROES;
+        return Promise.resolve(HEROES);
+    }
+    getHero(id: number) {
+        return Promise.resolve(HEROES).then(
+                heroes => heroes.filter(hero => hero.id === id)[0]
+        );
+    }
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+                setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+        );
     }
 }
